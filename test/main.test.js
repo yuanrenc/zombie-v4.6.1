c33334597971beb7ba6463fbe519998d133048d5
· <yuanrenc@gmail.com>
2022-04-02 13:23:28 +1100

    test bug fixed.

--- test/main.test.js
+++ test/main.test.js
@@ -13,14 +13,3 @@ describe(`Single Zombie move`, () => {
     ]);
   });
 });
-
-describe(`Single Zombie move`, () => {
-  test(`could return new Zombies list`, () => {
-    let zombie = { x: 2, y: 1, id: 0 };
-    let newId = 1;
-    expect(zombieMove(zombie, newId)).toEqual([
-      { id: 2, x: 1, y: 2 },
-      { id: 1, x: 2, y: 2 }
-    ]);
-  });
-});
